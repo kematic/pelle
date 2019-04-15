4a388b674f5f9ab9c0a0cde2b18ec7e694c2be4f
Kem Klattrup <45553531+kematic@users.noreply.github.com>
2019-04-15 21:44:02 +0200

    Update can_tkgui.py

--- can_tkgui.py
+++ can_tkgui.py
@@ -6,7 +6,6 @@ from threading import Thread, Event
 from datetime import *
 import time
 
-
 try:
     bus = can.interface.Bus(channel='can0', bustype='socketcan_native')
 except OSError:
@@ -17,14 +16,14 @@ q = queue.Queue()
 AcceptedNodes = [0x584, 0x604]
 
 
-class BaseThread(Thread):
+class bThread(Thread):
     def __init__(self):
         Thread.__init__(self)
         self.can_run = Event()
         self.term = Event()
         self.can_run.set()
-        self.term.clear()
-
+        self.term.clear()   
+    
     def pause(self):
         self.can_run.clear()
 
@@ -35,7 +34,7 @@ class BaseThread(Thread):
         self.term.set()
 
 
-class CanRxQueue(BaseThread):
+class can_rx_queue(bThread):
     def run(self):
         while True:
             if self.can_run.is_set():
@@ -46,34 +45,33 @@ class CanRxQueue(BaseThread):
                 break
 
 
-class QueueRead(BaseThread):
+class queue_read(bThread):
     def run(self):
         while True:
             if self.can_run.is_set():
                 if q.empty() is False:
                     message = q.get()
                     if message.arbitration_id in AcceptedNodes:
-                        d = '|{0}|{1:x}|{2:x}|'.format(datetime.fromtimestamp(message.timestamp),
-                                                       message.arbitration_id,
-                                                       message.dlc)
+                        d = '|{}|'.format(datetime.fromtimestamp(message.timestamp).time())
                         h = '{:x}:{:x}:{:x}:{:x}|'.format(message.data[0], message.data[1],
                                                           message.data[2], message.data[3])
                         b = '{:x}:{:x}:{:x}:{:x}|'.format(message.data[4], message.data[5],
                                                           message.data[6], message.data[7])
-                        w = '{}|'.format((message.data[5] << 8) + message.data[4])
+                        w1 = '{}+{}={}|'.format(message.data[4], message.data[5], (message.data[5] << 8) + message.data[4])
+                        w2 = '{}+{}={}|'.format(message.data[6], message.data[7], (message.data[7] << 8) + message.data[6])
                         dw = '{}|'.format((message.data[7] << 24) + (message.data[6] << 16) +
                                           (message.data[5] << 8) + message.data[4])
-                        print('{}{}{}{}{}\r\n'.format(d, h, b, w, dw))
-
+                        print('{}{}{}{}{}{}\r'.format(d, h, b, w1, w2, dw))
+            
             if self.term.is_set():
                 break
 
 
-class CanSend(BaseThread):
+class can_send(bThread):
     def __init__(self):
-        BaseThread.__init__(self)
+        bThread.__init__(self)
         self.can_run.clear()
-
+        
     def run(self):
         while True:
             if self.can_run.is_set():
@@ -83,7 +81,7 @@ class CanSend(BaseThread):
                                   extended_id=False)
                 bus.send(msg)
                 time.sleep(1)
-
+            
             if self.term.is_set():
                 break
 
@@ -103,13 +101,12 @@ def can_ctrl(cmd):
         b3Entry.config(state='normal')
 
 
-def stop_gui():
+def stop_GUI():
     s_can.terminate()
     t_can.terminate()
     t_queue.terminate()
     root.destroy()
     exit()
-
 
 root = Tk()
 
@@ -122,7 +119,7 @@ b2Entry = Entry(root)
 b3Entry = Entry(root)
 sendButton = Button(root, text="CAN - Start", command=lambda: can_ctrl(1))
 stopButton = Button(root, text="CAN - stop", command=lambda: can_ctrl(0))
-endButton = Button(root, text="Terminate GUI", command=lambda: stop_gui())
+endButton = Button(root, text="Terminate GUI", command=lambda: stop_GUI())
 
 b1Label.grid(row=0, column=0)
 b2Label.grid(row=0, column=1)
@@ -134,9 +131,9 @@ sendButton.grid(row=3, column=0)
 stopButton.grid(row=3, column=1)
 endButton.grid(row=3, column=2)
 
-t_can = CanRxQueue()
-t_queue = QueueRead()
-s_can = CanSend()
+t_can = can_rx_queue()
+t_queue = queue_read()
+s_can = can_send()
 
 
 def main():
